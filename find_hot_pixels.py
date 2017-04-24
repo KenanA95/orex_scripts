35d73b36d8a0fe0cd3ee9abf3b73476dd56d6d54
Alkiek <kalkiek@ndc.nasa.gov>
2017-04-24 10:19:58 -0400

    Started adding doc strings

--- find_hot_pixels.py
+++ find_hot_pixels.py
@@ -4,15 +4,29 @@ import numpy as np
 from collections import defaultdict
 
 
-# Find the overlap of active pixels that do not move between images and do not have active neighbors
+""" Check for hot/dead pixels in the NavCam images.
+
+Hot pixels are pixels that incorrectly show activity. Since the error exists on the hardware side,
+hot pixels will always remain in the same location. One idea that was implemented and later retracted
+was to identify the stars in the image and exclude their projected locations as potential hot pixels.
+The issue is that most of the images from a given set do not point at the same location. One possible solution
+is to identify the location of every single image, but that can be very computationally expensive and does
+not seem to produce better results.
+
+    Todo:
+        *
+        *
+"""
 
 
 def in_bounds(coords):
+    """Make sure the coordinate being evaluated is actually in the image. If an active pixel is found
+    on the very edge of the image, it's neighbor will be out of bounds"""
     return coords[0] < 1944 and coords[1] < 2592
 
 
-# Check if any of the neighboring pixels are above the active threshold
 def active_neighbor(im, coords, active_threshold):
+    """Check if any of the neighboring pixels are above the active threshold"""
     x, y = coords
 
     # Probably a better way to do this
@@ -26,9 +40,9 @@ def active_neighbor(im, coords, active_threshold):
     return False
 
 
-# Return a set of all x, y coordinates in an image above a given threshold
-# Ignore any pixel who's direct neighbors are also above the threshold
 def active_coordinates(im, active_threshold):
+    """Find the coordinates of every pixel above a given threshold. Ignore any who's neighbors are also active"""
+
     x, y = np.where(im >= active_threshold)
     active_coords = list(zip(x, y))
 
@@ -38,8 +52,8 @@ def active_coordinates(im, active_threshold):
     return set(active_coords)
 
 
-# Find the overlap of active pixels between images
 def active_overlap(navcam, sigma):
+    """Find the intersection of the active coordinates for every image in the set"""
     active_coords = {}
 
     for index, im in enumerate(navcam.images):
@@ -50,8 +64,8 @@ def active_overlap(navcam, sigma):
     return list(set.intersection(*active_coords.values()))
 
 
-# Get the value of every hot pixel across all images to compare statistics
 def get_hp_values(images, overlap):
+    """Get the value the hot pixels across all images for comparison statistics"""
     hp_values = defaultdict(list)
 
     for index, ol in enumerate(overlap):
@@ -61,6 +75,7 @@ def get_hp_values(images, overlap):
     return hp_values
 
 
+# Example usage
 if __name__ == "__main__":
 
     directory_day100 = 'C:/Users/kalkiek/Desktop/repos/data/navcam1/DAY100/'
@@ -75,7 +90,9 @@ if __name__ == "__main__":
     x_overlap = [o[1] for o in overlap]
     y_overlap = [o[0] for o in overlap]
 
-    # Display the overlap on a navcam image
-    plt.imshow(navcam1.images[20], cmap='gray', interpolation='nearest')
+    # Circle the locations on an empty image so that the people can overlay it onto new images
+    blank_active = np.zeros((1944, 2592))
+    plt.imshow(blank_active, cmap='gray', interpolation='nearest')
     plt.scatter(x_overlap, y_overlap, color='none', edgecolors='red', linewidths=1)
-    plt.show()
+    plt.axis('off')
+    plt.savefig('hot_pixels.png', bbox_inches='tight')
