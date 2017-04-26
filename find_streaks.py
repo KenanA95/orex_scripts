1c9e650f0af33bcaed16d12f77f89952bf656c0b
Alkiek <kalkiek@ndc.nasa.gov>
2017-04-26 14:32:59 -0400

    Light refactoring

--- find_streaks.py
+++ find_streaks.py
@@ -1,5 +1,4 @@
 from load_tagcam import load_tagcam
-from datetime import datetime, timedelta
 import numpy as np
 from skimage.feature import canny
 from skimage.transform import probabilistic_hough_line
@@ -9,27 +8,8 @@ __doc__ = """
 Identify potential streaks/particles in a set of images. The NavCam2 day 100 images exhibit a large number
 of streaks in the images and there is now a push to figure out the cause.
 
+Pre-process the images with a canny edge detector and use a hough transformation to identify lines in the images.
 """
-
-
-def parse_time(obsdate):
-    """ Take the date from the image header and format it into a datetime object
-
-    :param obsdate: The date in the format of year and fraction of year 2017.234324
-    :return: A datetime object holding the exact date and time the exposure began
-    """
-
-    fraction = str(obsdate)[4:]
-    days = float(fraction) * 365.25
-
-    # Just in case someone uses this in 2018
-    current_year = str(datetime.today().year)
-    start_date = datetime.strptime('01-01-' + current_year, '%d-%m-%Y')
-    time = start_date + timedelta(days=days)
-
-    # Adjust to UTC timezone
-    time = time - timedelta(hours=6)
-    return time
 
 
 def plot_lines(lines):
@@ -45,48 +25,50 @@ def distance(point_one, point_two):
     return np.sqrt((x2 - x1)**2 + (y2-y1)**2)
 
 
-# Most of the lines repeat next to each other. Only count the lines that are not neighbors
-# TODO Completely rewrite or remove entirely
-def count_unique_lines(lines):
+def count_streaks(lines, dist=25):
+    """ The hough transformation typically identifies multiple lines for a single streak. To count the individual
+    streaks, only count lines that are a given distance from one another.
 
-    unique_points = []
 
-    for line in lines:
-        starting_point = line[0]
-        # Add the first line in the list
-        if not unique_points:
-            unique_points.append(starting_point)
+    :param lines: The lines identified by the hough transformation
+    :param dist: Distance in pixels
+    :return: The number of streaks
+    """
+    starting_points = [l[0] for l in lines]
 
-        else:
-            is_unique = True
-            for point in unique_points:
-                if distance(starting_point, point) < 25:
-                    is_unique = False
+    # First line will always be unique
+    unique_points = [starting_points[0]]
 
-            if is_unique:
-                unique_points.append(starting_point)
+    for sp in starting_points:
+        unique = True
+
+        for point in unique_points:
+            if distance(sp, point) < dist:
+                unique = False
+
+        if unique:
+            unique_points.append(sp)
 
     return len(unique_points)
 
 
+# Example usage
 if __name__ == "__main__":
 
     directory = 'C:/Users/kalkiek/Desktop/repos/data/navcam2/DAY100/'
 
-    navcam2 = load_tagcam(directory)
+    navcam2 = load_tagcam([directory])
 
     for index, im in enumerate(navcam2.images):
-        # Correct orientation
-        im = np.fliplr(im)
 
         # No sigma because if you smooth the image you'll lose the dim streaks
         edges = canny(im, sigma=0)
         lines = probabilistic_hough_line(edges, threshold=5, line_length=7,
                                          line_gap=1)
 
-        time = parse_time(im.obsdate)
-        print("Index {0}: Streak Count {1}".format(index, count_unique_lines(lines)))
+        print("Index {0}: Streak Count {1}".format(index, count_streaks(lines)))
 
+        # Plot the streaks on the original image
         # plt.imshow(im, cmap='gray', interpolation='none')
         # plot_lines(lines)
         # plt.show()
--- stray_light.py
+++ stray_light.py
@@ -1,10 +1,14 @@
 from load_tagcam import load_tagcam
-from find_streaks import parse_time
 import numpy as np
 from matplotlib import pyplot as plt
 import matplotlib.patches as mpatches
+from datetime import datetime, timedelta
 
-__doc__ = """Plot stray light in the image corners over time to compare with orex sun angle """
+
+__doc__ = """Plot stray light in the image corners over time to compare with orex sun angle. The day 100 NavCam2 images
+exhibit an unusual amount of stray light in the corners.
+
+"""
 
 
 def get_grid(im, center, size):
@@ -21,6 +25,28 @@ def get_grid(im, center, size):
               center[1] - offset:center[1] + offset + 1]
 
 
+def parse_time(obsdate):
+    """ Take the date from the image header and format it into a datetime object
+
+    :param obsdate: The date in the format of year and fraction of year 2017.234324
+    :return: A datetime object holding the exact date and time the exposure began
+    """
+
+    fraction = str(obsdate)[4:]
+    days = float(fraction) * 365.25
+
+    # Just in case someone uses this in 2018
+    current_year = str(datetime.today().year)
+    start_date = datetime.strptime('01-01-' + current_year, '%d-%m-%Y')
+
+    # To convert the fraction to a date add it the start of the current year
+    time = start_date + timedelta(days=days)
+
+    # Adjust to UTC timezone
+    time = time - timedelta(hours=6)
+    return time
+
+
 # Example usage
 if __name__ == "__main__":
 
@@ -29,17 +55,19 @@ if __name__ == "__main__":
     navcam2 = load_tagcam(directories=[directory])
 
     for index, im in enumerate(navcam2.images):
-        # Correct orientation
+        # Correct orientation. Raw read function returns the image flipped
         im = np.fliplr(im)
+
+        y, x = im.shape
 
         # Grab a datetime representation of when the exposure begins
         time = parse_time(im.obsdate)
 
         # Grab the average DN value of each corner. Arbitrary grid size
         top_left = get_grid(im, (15, 15), size=31).mean()
-        top_right = get_grid(im, (15, 2592 - 15), size=31).mean()
-        bottom_left = get_grid(im, (1944 - 15, 15), size=31).mean()
-        bottom_right = get_grid(im, (1944 - 15, 2592 - 15), size=31).mean()
+        top_right = get_grid(im, (15, x - 15), size=31).mean()
+        bottom_left = get_grid(im, (y - 15, 15), size=31).mean()
+        bottom_right = get_grid(im, (y - 15, x - 15), size=31).mean()
 
         plt.plot(time, top_left, 'ro')
         plt.plot(time, top_right, 'go')
