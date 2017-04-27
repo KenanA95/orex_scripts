b5990549a6a18b7070654027a4bc1a58450a65c5
Alkiek <kalkiek@ndc.nasa.gov>
2017-04-27 12:43:02 -0400

    Code skeleton

--- tests/test_find_streaks.py
+++ tests/test_find_streaks.py
@@ -1,1 +1,21 @@
 import unittest
+import numpy as np
+from synthetic import Synthetic
+from find_streaks import find_streaks
+
+
+class TestFindStreaks(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_find_streaks(self):
+        pass
+
+
+# Helper function
+def add_streaks(images, count):
+    return
